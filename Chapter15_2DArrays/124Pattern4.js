0c98f277d7ff5665e35a172ef82d393e46eb234a
Vasudha <vasudhakulkarni2017@gmail.com>
2026-06-20 23:56:14 +0530

    Add Chapter15 2D arrays

---
+++ Chapter15_2DArrays/124Pattern4.js
@@ -0,0 +1,11 @@
+
+
+function printStarPattern(rows) {
+    for (let i = 1; i <= rows; i++) {
+        let row = '';
+        for (let j = 1; j <= i; j++) {
+            row += '* ';
+        }
+        console.log(row);
+    }
+}
